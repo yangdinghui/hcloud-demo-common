57e7032af5d3c3526d6ca2d076ba4c181fe312c4
ydh <1004329447@qq.com>
2020-04-01 18:18:06 +0800

    nio

--- src/main/java/hcloud/demo/netty/nio/client/NioClient.java
+++ src/main/java/hcloud/demo/netty/nio/client/NioClient.java
@@ -1,5 +1,12 @@
 package hcloud.demo.netty.nio.client;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+
 /**
  * description 描述这个类的主要功能、用途
  * 创建时间 2020-03-30
@@ -7,4 +14,21 @@ package hcloud.demo.netty.nio.client;
  * @author 杨丁辉
  */
 public class NioClient {
+
+    public static void main(String[] args) throws IOException {
+        Selector selector = Selector.open();
+        SocketChannel socketChannel = SocketChannel.open();
+        socketChannel.configureBlocking(false);
+
+        boolean isConnect = socketChannel.connect(new InetSocketAddress("192.168.33.104", 7397));
+        if (isConnect) {
+            socketChannel.register(selector, SelectionKey.OP_READ);
+        } else {
+            socketChannel.register(selector, SelectionKey.OP_CONNECT);
+        }
+        System.out.println("------------- nio client start done. -------------");
+        new NioClientHandler(selector, Charset.forName("GBK")).start();
+    }
+
 }
+
